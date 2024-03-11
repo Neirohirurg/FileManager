351d989b453509137abb6e87d3dc277dafbc9597
nik <n.kostrukov@mail.ru>
2024-03-11 20:04:43 +0300

    delete gitignore from this branch

--- FileManager/obj/Debug/net8.0/FileManager.AssemblyInfo.cs
+++ FileManager/obj/Debug/net8.0/FileManager.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("FileManager")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+238968e301bff8112889ba1e2fa1474f70206f43")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+77b5f61c332c66c87292729dd1487fe538317f06")]
 [assembly: System.Reflection.AssemblyProductAttribute("FileManager")]
 [assembly: System.Reflection.AssemblyTitleAttribute("FileManager")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
