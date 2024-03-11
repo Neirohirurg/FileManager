238968e301bff8112889ba1e2fa1474f70206f43
nik <n.kostrukov@mail.ru>
2024-03-11 19:56:08 +0300

    Добавьте файлы проекта.

---
+++ FileManager/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace FileManager
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
